41e93b7ea57e136415f254da43a6bb50ab390b63
Riley Hall <riley.hall.va@gmail.com>
2023-07-14 17:32:14 -0400

    Use setvars.sh

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -3,7 +3,7 @@
 #shellcheck disable=SC2010
 LATEST_VERSION=$(ls -1 /opt/intel/oneapi/compiler/ | grep -v latest | sort | tail -1)
 # shellcheck source=/dev/null
-source /opt/intel/oneapi/compiler/"$LATEST_VERSION"/env/vars.sh
+source /opt/intel/oneapi/setvars.sh
 
 echo -n "mpif90?   "
 echo $(which mpif90)
